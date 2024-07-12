0b53d6524ab895b534c6356760353256dde47883
Douglas <douglasvitor1233@gmail.com>
2024-07-11 22:13:29 -0300

    incomplete challenger

---
+++ 2562. Find the Array Concatenation Value/challenger.js
@@ -0,0 +1,10 @@
+var findTheArrayConcVal = function(nums){
+    
+
+    while(nums.length > 0){
+       let firstNum = nums[0]
+       let lastNum = nums[nums.length-1]
+       let numConcat = nums.con
+    }
+
+}
